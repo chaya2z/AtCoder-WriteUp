073a2904f193e8052f30dee9959c62948e233773
chaya2z <itunano57@gmail.com>
2020-10-06 09:53:00 +0900

    Add: B.py to abc01x/abc179
    Why: Solve abc179 task B

---
+++ abc/abc17x/abc179/B.py
@@ -0,0 +1,15 @@
+N = int(input())
+table = [[int(i) for i in input().split()] for _ in range(N)]
+
+cnt = 0
+
+for i in range(N):
+    if table[i][0] == table[i][1]:
+        cnt += 1
+        if cnt == 3:
+            print("Yes")
+            exit()
+    else:
+        cnt = 0
+
+print("No")
